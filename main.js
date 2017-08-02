11834f3b101f3a60a4bd98a15e32d5dd3949a320
lymanjohnson <lyman.johnson@gmail.com>
2017-08-02 13:05:01 -0400

    Block maker works

---
+++ main.js
@@ -0,0 +1,43 @@
+function createBio(datum) {  //receives ONE object, which will be customers.results[i]
+
+  let name    = datum.name.first + datum.name.last;
+  let email   = datum.email;
+  let street  = datum.location.street;
+  let city    = datum.location.city;
+  let state   = datum.location.state;
+  let zip     = datum.location.postcode;
+  let phone   = datum.phone;
+  let social  = datum.id.value;
+  let photoURL= datum.picture.medium;
+
+  let block ="";
+
+  block += "<li>";
+  block += `\n<img src="${photoURL}>"`
+  block += `\n<h2>${name}</h2>`;
+  block += `\n<h3 class ="email">${email}</h3>`
+  block += '\n<div class="address">'
+  block += `\n\t<h4 class="address-item">${street}</h4>`
+  block += `\n\t<h4 class="address-item">${city},${state} ${zip}</h4>`
+  block += `\n\t<h4 class="address-item">${phone}</h4>`
+  block += "\n</div>"
+  block += `\n<div class="ssn-wrapper"><h5 class = "ssn">${social}</h5></div>`
+  block += "\n </li>"
+
+  return(block);
+}
+
+console.log(createBio(customers.results[0]));
+
+
+
+
+
+// NAME (name) = datum.name.first  +   datum.name.last
+// EMAIL (email) = datum.email
+// ADDRESS (street / City, State / Phone)
+//   datum.location.street
+//   datum.location.city + datum.location.state + datum.location.postcode
+//   datum.phone
+// SSN (ssn) [*blurry*]
+//   datum.id.value
