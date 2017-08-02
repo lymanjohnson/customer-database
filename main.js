9af0729e409a4a49c29a06ca49ccd01816d33e06
lymanjohnson <lyman.johnson@gmail.com>
2017-08-02 14:46:02 -0400

    Final

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function createBio(datum) {  //receives ONE object, which will be customers.results[i]
 
-  let name    = datum.name.first + datum.name.last;
+  let name    = (datum.name.first + " "+ datum.name.last);
   let email   = datum.email;
   let street  = datum.location.street;
   let city    = datum.location.city;
@@ -14,11 +14,12 @@ function createBio(datum) {  //receives ONE object, which will be customers.resu
 
   block += "<li>";
   block += `\n<img src="${photoURL}">`
-  block += `\n<h2>${name}</h2>`;
+  block += `\n<h2 class="name">${name}</h2>`;
+  block += "<hr>"
   block += `\n<h3 class ="email">${email}</h3>`
   block += '\n<div class="address">'
   block += `\n\t<h4 class="address-item">${street}</h4>`
-  block += `\n\t<h4 class="address-item">${city},${state} ${zip}</h4>`
+  block += `\n\t<h4 class="address-item">${city}, ${state} ${zip}</h4>`
   block += `\n\t<h4 class="address-item">${phone}</h4>`
   block += "\n</div>"
   block += `\n<div class="ssn-wrapper"><h5 class = "ssn">${social}</h5></div>`
@@ -29,17 +30,16 @@ function createBio(datum) {  //receives ONE object, which will be customers.resu
 
 // console.log(createBio(customers.results[0]));
 let theList = document.getElementById('the-list');
-
+console.log(theList);
 
 function processData(data){  //receives the "customers" object
 
   let people = data.results;
 
   people.forEach( function(person){
-
-    console.log(person);
-    console.log(createBio(person));
-
+    // console.log(theList);
+    // console.log(person);
+    // console.log(createBio(person));
     theList.innerHTML += createBio(person);
 
   });
